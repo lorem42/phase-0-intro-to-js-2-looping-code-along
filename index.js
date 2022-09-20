6afd3e982dc627e6d18623da443aef8a9d1025ba
Sondos Ahmed Ibrahim <sunnysondos42@gmail.com>
2022-09-20 10:44:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const thankYou = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYou
+}
+function countDown(number) {
+    let count = number
+    while (count >= 0) {
+        console.log(count)
+        count--
+    }
+}
